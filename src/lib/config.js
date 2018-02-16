aac9990576dbeaeb3ded66fe5ac566f044782d3b
Kim Grytøyr <kim@grytoyr.io>
2018-02-16 18:12:47 +0100

    Moved version information to config lib file

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,12 +2,15 @@
 const fs = require('fs');
 
 const env = process.env.NODE_ENV || 'development';
+const VERSION = "v0.3.2";
 
 exports.getConfig = () => {
   const configFolder = process.env.CONFIG_PATH || '../data/';
 
   if (fs.existsSync(configFolder + 'config.' + env + '.json')) {
-    return JSON.parse(fs.readFileSync('../data/config.' + env + '.json'));
+    const conf = JSON.parse(fs.readFileSync('../data/config.' + env + '.json'));
+    conf.version = VERSION;
+    return conf;
   } else {
     throw Error('Unable to read config..');
   }
