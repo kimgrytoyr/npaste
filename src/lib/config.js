48ade96bd0f2525a789df1664f17caf7fc39af63
Kim Grytøyr <kim@grytoyr.io>
2018-02-19 23:15:26 +0100

    Config files now using YAML. See dev config for comments

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,14 +1,22 @@
 // npm modules
 const fs = require('fs');
+const yaml = require('js-yaml');
 
 const env = process.env.NODE_ENV || 'development';
-const VERSION = "v0.4";
+const VERSION = "v0.4.2";
 
 exports.getConfig = () => {
   const configFolder = process.env.CONFIG_PATH || '../data/';
 
-  if (fs.existsSync(configFolder + 'config.' + env + '.json')) {
-    const conf = JSON.parse(fs.readFileSync('../data/config.' + env + '.json'));
+  if (fs.existsSync(configFolder + 'config.' + env + '.yml')) {
+    let conf = {};
+    try {
+      conf = yaml.safeLoad(fs.readFileSync('../data/config.' + env + '.yml'));
+    }
+    catch (e) {
+      throw Error('Unable to parse config..');
+    }
+
     conf.version = VERSION;
     return conf;
   } else {
