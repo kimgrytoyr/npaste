4aa153d7072983434376ecc62185d70b377a3363
Kim Grytøyr <kim@grytoyr.io>
2019-12-05 08:17:08 +0100

    Remove excessive whitespace

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@
 // npm modules
 const express = require('express');
 const router = express.Router({ strict: true });
-const multer  = require('multer');
+const multer = require('multer');
 const config = require('../lib/config').getConfig(); // local
 const upload = multer({ dest: config.path })
 
